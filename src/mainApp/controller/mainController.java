6ee96e15722e0f2f39d5f259c5613eece71476d1
Raúl Herrerías Corral <raulhc1313@gmail.com>
2022-06-10 20:10:09 +0200

    Funcionalidad, sin excepciones

--- src/mainApp/controller/mainController.java
+++ src/mainApp/controller/mainController.java
@@ -1,6 +1,9 @@
 package mainApp.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,40 +25,51 @@ public class mainController {
 	
 	private ObservableList<String> obsGanadores = FXCollections.observableArrayList();
 	
-	private HashSet<String> hashSetParticipantes = new HashSet<>();
-	
 	@FXML
 	private void salir(ActionEvent evento) {
 		((Stage) BTSalir.getScene().getWindow()).close();
 	}
 	
-	public ObservableList<String> getParticipantes() {
+	public HashSet<String> getParticipantes() {
+		
+		HashSet<String> hashSetParticipantes = new HashSet<>();	
+		
+		hashSetParticipantes.removeAll(hashSetParticipantes);
 		
 		String [] participantes = TAParticipantes.getText().split("\\R");
 		
+		for (String participante : participantes) {
+			hashSetParticipantes.add(participante);
+		}
 		
-		obsGanadores = FXCollections.observableArrayList(participantes);
+		return hashSetParticipantes;
+	}
+	
+	private ObservableList<String> getGanadores() {
+		
+		ArrayList<String> participantesLista = new ArrayList<>(getParticipantes());
+		
+		Collections.shuffle(participantesLista);
+		
+		List<String> resultado = participantesLista.subList(0, getNumParticipantes());
+		
+		obsGanadores = FXCollections.observableArrayList(resultado);
 		
 		return obsGanadores;
 	}
 	
-	
-	
-	private void getGanadores() {
-		
-	}
-	
 	@FXML public void setResultado() {
 		
-		LVGanadores.setItems(getParticipantes());
+		LVGanadores.setItems(getGanadores());
 	}
 	
 	private Integer getNumParticipantes() {
+		
 		int num = 0;
 		
 		try {
 			
-		 num = Integer.parseInt(TAParticipantes.getText());
+		 num = Integer.parseInt(TFNParticipantes.getText());
 		 
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
@@ -63,5 +77,4 @@ public class mainController {
 		
 		return num;
 	}
-	
 }
